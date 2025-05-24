3099aff40da3db23f87a1f345a6fa4c2a0b98667
Макар <20makbul12@gmail.com>
2025-05-24 19:01:08 +0300

    Это база444

--- Agar-agario/Client.py
+++ Agar-agario/Client.py
@@ -17,11 +17,30 @@ pygame.display.set_caption('Agar-agario')
 radplay = 30
 oldvect = (0, 0)
 
+def checkvect(mess):
+    first = None
+    for ind, lett in enumerate(mess):
+        if lett == "<":
+            first = ind
+        if lett == ">" and first is not None:
+            second = ind
+            result = mess[first + 1: second].split(",")
+            return result
+
+def draw(data):
+    x, y, size, color = data.split()
+    x = WIDTH // 2 + int(x)
+    y = HEIGHT // 2 + int(y)
+    size = int(size)
+    pygame.draw.circle(surface=window, color=color, center=(x, y), radius=size)
+
+
 run = True
 while run:
     try:
         mess = master.recv(1024).decode()
-        #print(mess)
+        print(mess)
+        victims = checkvect(mess)
     except(BlockingIOError, ConnectionResetError, ConnectionAbortedError):
         #print("800fps")
         pass
@@ -30,7 +49,7 @@ while run:
         mx, my = pygame.mouse.get_pos()
         vx, vy = mx - WIDTH // 2, my - HEIGHT // 2
         vecd = (vx**2 + vy**2)**0.5
-        print(vx, vy)
+        #print(vx, vy)
         if vecd < radplay:
             vx, vy = 0, 0
         else:
@@ -49,6 +68,12 @@ while run:
             run = False
 
     window.fill("grey")
-    pygame.draw.circle(surface=window, color="yellow", center=(WIDTH // 2, HEIGHT //2), radius=radplay)
+
+    print(victims)
+    for i in victims:
+        if i:
+            draw(i)
+
+    pygame.draw.circle(surface=window, color="yellow", center=(WIDTH // 2, HEIGHT // 2), radius=radplay)
     pygame.display.flip()
 pygame.quit()
--- Agar-agario/Server.py
+++ Agar-agario/Server.py
@@ -10,33 +10,51 @@ master.setblocking(False)
 master.listen(5)
 print("Сокет создан!")
 users = {}
-class Gamer():
+colors = ["white", "purple", "black", "green", "red", "blue", "yellow", "orange"]
+class Gamer:
     def __init__(self, name, sk, adress, id):
         self.id = id
         self.name = "wizarD"
         self.sk = sk
         self.adress = adress
-        self.x = 250
-        self.y = 250
-        self.xspeed = 2
-        self.yspeed = 2
-        self.abspeed = 3
+        self.x = 2500
+        self.y = 2500
+        self.xspeed = 1
+        self.yspeed = 1
+        self.abspeed = 5
         self.color = "black"
         self.size = 100
         self.db = session.get(Stats, self.id)
         self.xvid = 500
         self.yvid = 500
     def move(self):
+        x = self.x + self.xspeed * self.abspeed
+        y = self.y + self.yspeed * self.abspeed
+        if x <= 0 or x >= WWIDTH:
+            self.xspeed = 0
+        if y <= 0 or y >= WHEIGHT:
+            self.yspeed = 0
+        #if self.adress:
+            #print(self.x, self.y, self.xspeed, self.yspeed)
         self.x += self.xspeed * self.abspeed
         self.y += self.yspeed * self.abspeed
+
     def changesp(self, vx, vy):
         self.xspeed = float(vx)
         self.yspeed = float(vy)
 
 
+class Food:
+    def __init__(self):
+        self.x = random.randint(0, WWIDTH)
+        self.y = random.randint(0, WHEIGHT)
+        self.size = 18
+        self.color = random.choice(colors)
+
+
 WIDTH = 500
 HEIGHT = 500
-FPS = 20
+FPS = 30
 WWIDTH = 5000
 WHEIGHT = 5000
 
@@ -45,6 +63,7 @@ pygame.init()
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Server')
 clock = pygame.time.Clock()
+foods = []
 
 def checkvect(mess):
     first = None
@@ -59,6 +78,14 @@ def checkvect(mess):
 def vision(p1: Gamer, p2: Gamer):
     distx = p2.x - p1.x
     disty = p2.y - p1.y
+    if abs(distx) <= p1.xvid / 2 + p2.size and abs(disty) <= p1.yvid / 2 + p2.size:
+        x = int(distx)
+        y = int(disty)
+        size = int(p2.size)
+        color = p2.color
+        data = f"{x} {y} {size} {color}"
+        usvid[p1.id].append(data)
+
 
 for i in range(50):
     bot = Gamer(name="boba", adress=None, sk=None, id=i)
@@ -70,31 +97,30 @@ for i in range(50):
     bot.yspeed = random.random() * 2 - 1
     users[i] = bot
 
+for i in range(300):
+    eda = Food()
+    foods.append(eda)
+
+tick = 0
 run = True
 while run:
     clock.tick(FPS)
-    try:
-        clients, adres = master.accept()
-        clients.setblocking(False)
-        print(adres, "Подключился")
-        player = Stats(adress=adres)
-        session.merge(player)
-        session.commit()
-
-
-        host = f"({adres[0]},{adres[1]})"
-        data = session.query(Stats).filter(host==Stats.adress).all()[0]
-        user = Gamer(name="John",sk=clients,adress=host,id=data.id)
-        users[user.id] = user
-    except BlockingIOError:
-        pass
-
-
-    for i in users:
+    tick += 1
+    if tick % FPS * 5 == 0:
         try:
-            if users[i].sk is not None:
-                users[i].sk.send("Я тут".encode())
-        except(BlockingIOError, ConnectionResetError, ConnectionAbortedError):
+            clients, adres = master.accept()
+            clients.setblocking(False)
+            print(adres, "Подключился")
+            player = Stats(adress=adres)
+            session.merge(player)
+            session.commit()
+
+            host = f"({adres[0]},{adres[1]})"
+            data = session.query(Stats).filter(host==Stats.adress).all()[0]
+            user = Gamer(name="John",sk=clients,adress=host,id=data.id)
+            user.abspeed = 15
+            users[user.id] = user
+        except BlockingIOError:
             pass
 
     for i in users:
@@ -117,7 +143,23 @@ while run:
     for i in range(len(prs)):
         p1 = prs[i][1]
         for a in range(i+1, len(prs)):
-            p2 = prs[a]
+            p2 = prs[a][1]
+            vision(p1, p2)
+            vision(p2, p1)
+            for i in foods:
+                vision(i, p1)
+
+    for i in users:
+        usvid[i] = ",".join(usvid[i])
+        usvid[i] = f"<{usvid[i]}>"
+
+    for id in users:
+        try:
+            if users[id].sk is not None:
+                mss = usvid[id]
+                users[id].sk.send(mss.encode())
+        except(BlockingIOError, ConnectionResetError, ConnectionAbortedError):
+            pass
 
     events = pygame.event.get()
     for event in events:
